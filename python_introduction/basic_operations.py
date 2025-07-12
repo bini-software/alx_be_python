690f44972ac27d6b84dc7744ac85663faf638fbf
Biniyam Girma <biniswengineer@gmail.com>
2025-07-12 16:15:19 +0300

    Add bassic arithmetic operations script

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,6 +1,10 @@
 number1 = 10
 number2 = 5
-
+sum_result = number1 + number2
 print(f"Addition of {number1} and {number2} is {number1 + number2}")
+
+difference = number1 - number2
 print(f"subtraction of {number1} and {number2} is {number1 - number2}")
+
+product = number1 * number2
 print(f"Multiplication of {number1} and {number2} is {number1 * number2}")
